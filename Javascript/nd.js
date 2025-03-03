9d854c81d365d817e55a5065e1a2e9660aa2ca83
Cripsipriksi23 <64037374+Cripsipriksi23@users.noreply.github.com>
2025-03-03 19:24:40 +0530

    javascript done

--- Javascript/nd.js
+++ Javascript/nd.js
@@ -1,2 +1,21 @@
-console.log(" through node js ")
-console.log("hi this is the 2nd time")
+// let karr=[]
+// Array.prototype.mypush = function(...k_ele){
+//     for(let i=0;i<k_ele.length;i++){
+//         this[this.length]=k_ele[i]
+//     }
+//     return k_ele.length
+// }
+// karr.mypush(12,34,56)
+// let p = karr[0]
+// console.log("real value of p: "+p)
+// console.log(typeof p)
+
+// let value_p= Boolean(p)
+
+// console.log(typeof value_p)
+// console.log( value_p)
+
+p="90"
+k=Number(p)
+console.log(typeof k)
+console.log(k+91)
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,16 +1,38 @@
-let d={
-    ecstatic:"something diff",
-    insensitive:"indifferent",
-    dev:"developer",
-    num:8826
+// let d={
+//     ecstatic:"something diff",
+//     insensitive:"indifferent",
+//     dev:"developer",
+//     num:8826
+// }
+// s1=8
+// s2=4
+// s3=17
+// if(s1<s2+s3 && s2<s1+s3 && s3<s1+s2 ){
+//     console.log("true11","true22,","true33")
+//     console.log("its a triangle")
+// }
+// else{
+//     console.log("not a triangle")
+// }
+// let i="joker got killed by batman"
+// console.table([s1,s2,s3,i])
+Array.prototype.mypush= function(...l){
+    for (let i=0;i<l.length;i++){
+        this[this.length] = l[i]
+    }
+    return this.length
 }
-s1=8
-s2=4
-s3=17
-if(s1<s2+s3 && s2<s1+s3 && s3<s1+s2 ){
-    console.log("true11","true22,","true33")
-    console.log("its a triangle")
+mun=[57,55]
+mun.mypush(89,98,0)
+//console.log(mun)
+Array.prototype.mypop= function(p){
+    if (this.length===0){
+        return undefined
+    }
+    else{
+        this.length= this.length-1
+        return this.length;
+    }
 }
-else{
-    console.log("not a triangle")
-}
+mun=mun.mypop()
+console.log(mun)
